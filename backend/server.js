e556b8b2d1183548e5fa05eee0e2b796d989363b
Ahmed <rmmhnrnrjr@gmail.com>
2026-01-17 18:58:46 +0200

    handle deployment

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,15 @@ import userRouter from './src/router/user.router.js';
 import cartRouter from './src/router/cart.router.js';
 import orderRouter from './src/router/order.router.js';
 import "dotenv/config";
-
-
+import serverless from 'serverless-http'; // <-- added
 
 // app Config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
-
 
 // api endPoints
 app.use('/api/food', foodRouter);
@@ -25,20 +23,19 @@ app.use('/api/auth', userRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
 
-
-
 // db connection
 connectDB();
-
 
 app.get('/', (req, res) => {
   res.send('Hello World! api works ');
 });
 
+// only use app.listen locally
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
+}
 
-// listen {test server}
-app.listen(port, () => {
-  console.log(`app is listen on port ${port}`);
-
-});
-
+// export serverless handler for Vercel
+export const handler = serverless(app);
